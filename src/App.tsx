0e44f8fc19dede37c0830b17f7ba0d40116c26d4
Jokernix <spirin.egor13@gmail.com>
2023-09-25 22:02:05 +0300

    feat: add tanstack virtualized

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import NoticeExample from "./components/Examples/NoticeExample";
 import TipExample from "./components/Examples/TipExample";
 import { NoticeProvider } from "./components/Notifications/NoticeProvider";
 import DynamicHeightGrid from "./components/ReactVirtualized/ReactVirtualized";
+import TanstackVirtualized from "./components/TanstackVirtualized";
 import TipPositionHelper from "./components/Tip/TipPositionHelper";
 
 function App() {
-  return <DynamicHeightGrid />
+  return <TanstackVirtualized />
   return (
     <NoticeProvider>
       <div className="flex flex-col overflow-auto h-screen px-4 py-2 gap-4">
---
+++ src/components/TanstackVirtualized.tsx
@@ -0,0 +1,123 @@
+import { faker } from '@faker-js/faker'
+
+import { useVirtualizer, Virtualizer } from '@tanstack/react-virtual'
+import useDebounce from '../hooks/useDebounce'
+import { useRef, useState } from 'react'
+
+
+const randomNumber = (min: number, max: number) =>
+  faker.number.int({ min, max })
+
+const sentences = new Array(10000)
+  .fill(true)
+  .map(() => faker.lorem.sentence(randomNumber(20, 70)))
+
+export default function RowVirtualizerDynamic() {
+  const parentRef = useRef<HTMLDivElement>(null)
+  const [cache, setCache] = useState<Record<number, string>>({})
+
+  const handleChange = async (instance: Virtualizer<HTMLDivElement, Element>) => {
+    const { startIndex, endIndex } = instance.range
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments/2')
+    const data = await res.json()
+    const str = data.body + data.name
+
+    let newData: Record<number, string> = {}
+    for (let i = startIndex; i <= endIndex; i++) {
+      newData[i] = str
+    }
+
+    setCache(p => ({ ...p, ...newData }))
+  }
+  console.log(cache)
+  const handleChangeDebounce = useDebounce(handleChange, 500)
+
+  const count = sentences.length
+  const virtualizer = useVirtualizer({
+    count,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 120,
+    onChange: handleChangeDebounce
+  })
+
+  const items = virtualizer.getVirtualItems()
+
+  return (
+    <div className='w-screen h-screen flex flex-col'>
+      <div className='flex'>
+        <button
+          onClick={() => {
+            virtualizer.scrollToIndex(0)
+          }}
+        >
+          scroll to the top
+        </button>
+        <span style={{ padding: '0 4px' }} />
+        <button
+          onClick={() => {
+            virtualizer.scrollToIndex(count / 2)
+          }}
+        >
+          scroll to the middle
+        </button>
+        <span style={{ padding: '0 4px' }} />
+        <button
+          onClick={() => {
+            virtualizer.scrollToIndex(count - 1)
+          }}
+        >
+          scroll to the end
+        </button>
+      </div>
+      <hr />
+      <div
+
+        ref={parentRef}
+        className="grow"
+        style={{
+          height: "100%",
+          width: '100%',
+          overflowY: 'auto',
+          contain: 'strict',
+        }}
+      >
+        <div
+          style={{
+            height: virtualizer.getTotalSize(),
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              transform: `translateY(${items[0].start}px)`,
+            }}
+          >
+            {items.map((virtualRow) => {
+              const isItemLoaded = cache.hasOwnProperty(virtualRow.index)
+
+              return <div
+                key={virtualRow.key}
+                data-index={virtualRow.index}
+                ref={virtualizer.measureElement}
+                className={
+                  virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'
+                }
+              >
+                {!isItemLoaded && <div>Loading...</div>}
+                {isItemLoaded && <div style={{ padding: '10px 0' }}>
+                  <div>{cache[virtualRow.index]}</div>
+                </div>}
+              </div>
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
